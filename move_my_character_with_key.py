15c81d2bf076527808776202c8d2fe9eda00dbd8
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-09-25 16:15:10 +0900

    added KEY_DOWN LEFT, KEY_UP LEFT

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -9,6 +9,8 @@ from pico2d import *
 ground_width, ground_height = 1280, 1024
 running = True
 frame = 0
+x, y = ground_width // 2, ground_height // 2
+xdir, ydir = 0, 0
 
 open_canvas(ground_width, ground_height)
 ground = load_image('TUK_GROUND.png')
@@ -16,19 +18,28 @@ character = load_image('character.png')
 
 def handle_events() :
     global running
+    global xdir, ydir
     events = get_events()
     for event in events :
-        if (event.type == SDL_KEYDOWN) :
+        if (event.type == SDL_QUIT) :
+            running = False
+        elif (event.type == SDL_KEYDOWN) :
+            if (event.key == SDLK_LEFT) :
+                xdir -= 1
             if (event.key == SDLK_ESCAPE) :
-                running = False
+                running = False 
+        elif (event.type == SDL_KEYUP) :
+            if (event.key == SDLK_LEFT) :
+                xdir += 1
 
 while(running) :
     clear_canvas()
     ground.draw(ground_width // 2, ground_height // 2)
-    character.clip_draw(frame * 64, 64, 64, 64, ground_width // 2, ground_height // 2)
+    character.clip_draw(frame * 64, 0, 64, 64, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 4
-    delay(1)
+    x += xdir * 5
+    delay(0.1)
 
 close_canvas()
