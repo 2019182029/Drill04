50bfa2f7d22a912e7267f5b48df915131a54e400
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-09-25 15:49:42 +0900

    Create Base

---
+++ move_my_character_with_key.py
@@ -0,0 +1,19 @@
+# 1. 상하좌우 방향키를 이용하여 이동
+# 2. 캐릭터가 작을 경우 확대
+
+# 배경 : TUK_GROUND.png
+# 캐릭터 : character.png
+
+from pico2d import *
+
+open_canvas()
+ground = load_image('TUK_GROUND')
+character = load_image('character.png')
+
+def handle_events() :
+    pass
+
+while(running) :
+    pass
+
+close_canvas()
